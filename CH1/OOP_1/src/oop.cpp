5d801e82228293bd1ab48bf146ef9b02874f5cb8
Erdem <erdemcanaz@gmail.com>
2024-10-30 22:54:22 +0300

    .

--- CH1/OOP_1/src/oop.cpp
+++ CH1/OOP_1/src/oop.cpp
@@ -2,20 +2,40 @@
 #include <string>
 
 class Person{
-    public:
+    //private by default
+    private:
         std::string first;
         std::string last;
 
+    public:
+        void setFirstName(std::string first_name){
+           first = first_name;
+        }
+        void setLastName(std::string last){
+            this->last = last;
+        }
+        std::string getName(){
+            return first+ " " + last;
+        }
         void printFullName(){
             std::cout << first << " " << last << std::endl;
         }
+
 };
 
 int main(){
     Person p;
-    p.first = "Caleb";
-    p.last = "Curry";
-   
+    p.setFirstName("Stephen");
+    p.setLastName("Curry");   
     p.printFullName();
+
+    Person p2;
+    p2.setFirstName("John");
+    p2.setLastName("Doe");
+    p2.printFullName();
+
+    std::cout << p2.getName() << std::endl;
+
+    
     return 0;
 }
