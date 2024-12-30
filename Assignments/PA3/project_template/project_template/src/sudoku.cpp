9abc4d4586b84c037ad1c9f082354bc05e6dbe24
Erdem <erdemcanaz@gmail.com>
2024-12-30 19:26:06 +0300

    changes on sudoku file is reversed

--- Assignments/PA3/project_template/project_template/src/sudoku.cpp
+++ Assignments/PA3/project_template/project_template/src/sudoku.cpp
@@ -1,70 +1,28 @@
 #include <iostream>
-#include <stdexcept>
 #include "sudoku.hpp"
 #include "memory.hpp"
 
-Sudoku::Sudoku() : Graph(create("data/sudoku.txt"))
+Sudoku::Sudoku()
 {
-    // Nothing else needed here since the base class is initialized
+    throw std::logic_error("Function \"Sudoku constructor\" is not implemented!");
 }
 
 Sudoku::~Sudoku()
 {
-    // Nothing special to do, as the Graph base destructor
-    // takes care of cleaning up dynamic memory for the vertices.
+    throw std::logic_error("Function \"Sudoku destructor\" is not implemented!");
 }
 
 bool Sudoku::solve()
 {
-    // For Sudoku, the Graph::color() method tries to color all vertices
-    // from 1..max_color() in a backtracking manner.
-    // If it succeeds, we have a solution; otherwise, no solution.
-    return this->color();
+    throw std::logic_error("Function \"Sudoku solve\" is not implemented!");
 }
 
 void Sudoku::print()
 {
-    // Print the Sudoku board in 9 rows x 9 columns
-    // Each vertex ID from 1..81 corresponds to row,col as:
-    //   row = (id-1) / 9
-    //   col = (id-1) % 9
-    //
-    // We add some spacing/lines to mimic a Sudoku layout.
-
-    for (int row = 0; row < 9; ++row)
-    {
-        // Horizontal line every 3 rows (except the top might be omitted or customized)
-        if (row % 3 == 0 && row != 0)
-        {
-            std::cout << "------+-------+------" << std::endl;
-        }
-
-        for (int col = 0; col < 9; ++col)
-        {
-            // Vertical line every 3 columns (except the leftmost can be omitted or customized)
-            if (col % 3 == 0 && col != 0)
-            {
-                std::cout << " |";
-            }
-
-            // Compute the cell ID in [1..81]
-            int id = row * 9 + col + 1;
-            int c = (*this)[id]->color();
-
-            // If color == 0, that means uncolored
-            if (c == 0)
-                std::cout << " .";
-            else
-                std::cout << " " << c;
-        }
-        std::cout << std::endl;
-    }
+    throw std::logic_error("Function \"Sudoku print\" is not implemented!");
 }
 
 int Sudoku::max_color() const
 {
-    // For a standard Sudoku, we always want digits 1..9
-    // regardless of what the underlying graph says about "maximum degree + 1".
-    // So we override the base class method to forcibly return 9.
-    return 9;
+    throw std::logic_error("Function \"Sudoku max_color\" is not implemented!");
 }
--- Assignments/PA3/project_template/project_template/src/sudoku.hpp
+++ Assignments/PA3/project_template/project_template/src/sudoku.hpp
@@ -7,11 +7,12 @@ class Sudoku : private Graph
 {
 private:
     int max_color() const override;
+
 public:
     Sudoku();
     ~Sudoku();
 
-    void set_clues(const char* filename);
+    void set_clues(const char *filename);
     bool solve();
     void print();
 };
